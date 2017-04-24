59c0396954bca89b85c323e92b05ead21ac1d3ea
Gauthiervangre <vangrevelynghe.gauthier@hotmail.com>
2017-04-24 12:25:58 +0200

    Add files via upload

---
+++ map.mm
@@ -0,0 +1,78 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Page de connexion" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1490943326626"><hook NAME="MapStyle" zoom="0.909">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<node TEXT="Accueil" POSITION="right" ID="ID_660773527" CREATED="1490943405056" MODIFIED="1490943888347" HGAP="-250" VSHIFT="50">
+<edge COLOR="#ff0000"/>
+<node TEXT="Profil" ID="ID_193996286" CREATED="1490943425283" MODIFIED="1490943435064" HGAP="-170" VSHIFT="-90">
+<node TEXT="Voir et modifier ses informations personnelles" ID="ID_1114200706" CREATED="1490943438380" MODIFIED="1490943496835" HGAP="-420" VSHIFT="-70"/>
+<node TEXT="Cr&#xe9;diter son compte" ID="ID_1171516073" CREATED="1490943451820" MODIFIED="1490943462459" HGAP="-240" VSHIFT="-30"/>
+<node TEXT="Consulter son solde" ID="ID_854652901" CREATED="1490943465759" MODIFIED="1490943477631" HGAP="-240" VSHIFT="-70"/>
+</node>
+<node TEXT="Mes vols" ID="ID_748922964" CREATED="1490943499261" MODIFIED="1490943520275" HGAP="-200" VSHIFT="-20">
+<node TEXT="Voir les vols sans forfait du membre" ID="ID_180379837" CREATED="1490943525946" MODIFIED="1490943540985" HGAP="-300" VSHIFT="-70"/>
+<node TEXT="Voir les vols avec forfait du membre" ID="ID_1697846385" CREATED="1490943543224" MODIFIED="1490943557156" HGAP="-340"/>
+</node>
+<node TEXT="Mes forfaits" ID="ID_1759791196" CREATED="1490943560174" MODIFIED="1490943564667" HGAP="-170" VSHIFT="50">
+<node TEXT="Voir mes forfaits actifs" ID="ID_1403704897" CREATED="1490943568560" MODIFIED="1490943577358" HGAP="-260" VSHIFT="10"/>
+<node TEXT="Voir mes forfaits termin&#xe9;s" ID="ID_1554738968" CREATED="1490943579082" MODIFIED="1490943592936" HGAP="-290" VSHIFT="40"/>
+<node TEXT="Acheter un forfait sur un avion" ID="ID_440710313" CREATED="1490943607343" MODIFIED="1490943717794" HGAP="-290" VSHIFT="50"/>
+</node>
+<node TEXT="Op&#xe9;rations sur compte" ID="ID_778746024" CREATED="1490943719471" MODIFIED="1490943740703" HGAP="-220" VSHIFT="100">
+<node TEXT="Voir les op&#xe9;rations effectu&#xe9;es sur le compte" ID="ID_1954038076" CREATED="1490943750094" MODIFIED="1490943865617" HGAP="-470">
+<node TEXT="Cr&#xe9;ditation du compte" ID="ID_626305969" CREATED="1490943896919" MODIFIED="1490943904828" HGAP="-442" VSHIFT="-33"/>
+<node TEXT="Achat de vol" ID="ID_1532470346" CREATED="1490943908175" MODIFIED="1490943917176" HGAP="-442" VSHIFT="11"/>
+<node TEXT="Achat de forfait" ID="ID_1386437733" CREATED="1490943918962" MODIFIED="1490943930818" HGAP="-453" VSHIFT="44"/>
+</node>
+</node>
+</node>
+</node>
+</map>
